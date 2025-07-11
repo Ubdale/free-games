a618db5fbbbf8770101848625fd4c25ce3e54c70
Muhammad Ubdale <99432158+Ubdale@users.noreply.github.com>
2025-07-11 06:26:21 -0700

    fixing route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { FreeGamesComponent } from './modules/free-games/free-games.component';
 
 export const routes: Routes = [
+    {path:'**', component: FreeGamesComponent },
     {path:'free-games', component: FreeGamesComponent }
 ];
